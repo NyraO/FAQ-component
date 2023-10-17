25c2e9fa79b6c15757bc51a8f759537542328ec8
NyraO <99379453+NyraO@users.noreply.github.com>
2023-10-17 14:20:53 +0300

    final js

--- myScript.js
+++ myScript.js
@@ -1,13 +1,13 @@
 
 class Accordion{
     toggle=false;
-        arrow_down(){
+        arrow_down(containerId, arrowId){
         this.toggle=!this.toggle;
-        this.toggle?this.show_answer():this.hide_answer();
+        this.toggle?this.show_answer(containerId, arrowId):this.hide_answer(arrowId);
         }
-        show_answer(){
-            const element=document.getElementById("que-container"); 
-            const downArrow=document.getElementById("arrow");
+        show_answer(containerId, arrowId){
+            const element=document.getElementById(containerId); 
+            const downArrow=document.getElementById(arrowId);
             const para = document.createElement("p");
             para.id='new-paragraph';
             para.style.display="block";
@@ -16,10 +16,10 @@ class Accordion{
             element.appendChild(para);
             downArrow.style.transform="rotate(180deg)";
         }
-        hide_answer(){
+        hide_answer(arrowId){
             const newElement = document.getElementById('new-paragraph');
             newElement.remove();
-            const downArrow=document.getElementById("arrow");
+            const downArrow=document.getElementById(arrowId);
             downArrow.style.transform="rotate(360deg)";
         }
 }
